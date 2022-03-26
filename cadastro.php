54b98c0d272c88b5b0f9fb2e39e6e4981b3e6993
Andrik Machado Batista <andrikmb@gmail.com>
2022-03-25 22:02:23 -0300

    modificando formulario

--- cadastro.php
+++ cadastro.php
@@ -6,16 +6,15 @@ include __DIR__.'/includes/header.php';
 <form>
 	  Nome:<br/> 
 	  <input type="text" name="nome" placeholder="Qual seu nome?"><br/><br/>
-	  E-mail:<br/> 
-	  <input type="email" name="email" placeholder="Qual seu e-mail?"><br/><br/>
-	  Cidade:<br/> 
-	  <input type="text" name="cidade" placeholder="Qual sua cidade?"><br/><br/>
-	  UF:<br/> 
-	  <input type="text" name="uf" size="2" placeholder="UF">
+	  Login:<br/> 
+	  <input type="text" name="login" placeholder="Qual login deseja usar?"><br/><br/>
+	  E-mail
+	  <input type="email" name="email" placeholder="Qual seu email?"><br/><br/>
 	  <br/><br/>
 	  <input type="hidden" value="-1" name="id" >
 	  <button type="submit">Cadastrar</button>
 	</form>
+    
     <?php 
     include __DIR__.'/includes/footer.php';
     ?>
