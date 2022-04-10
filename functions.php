47e8f99ce1cfc1ee86db8d84290ff8b723b5942e
Andrik Machado Batista <andrikmb@gmail.com>
2022-04-10 19:17:47 -0300

    construindo classe cliente

--- functions.php
+++ functions.php
@@ -7,10 +7,24 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 
 class Cliente {
-function create(){}
-function read(){}
-function update(){}
-function delete(){}
+    
+    //    `id` int(11) NOT NULL,
+    private int $id=0;
+    //    `nome` varchar(255) COLLATE utf8_bin NOT NULL,
+    private string $nome="";
+    //    `cpf_cnpj` varchar(14) COLLATE utf8_bin NOT NULL,
+    private string $cpf_cnpj="";
+    //    `email` varchar(255) COLLATE utf8_bin NOT NULL,
+    private string $email="";
+    //    `criacao` varchar(255) COLLATE utf8_bin NOT NULL,
+    private string $criacao="";
+    //    `modificado` varchar(255) COLLATE utf8_bin NOT NULL,
+    private string $modificado="";
+    
+    function create(){}
+    function read(){}
+    function update(){}
+    function delete(){}
 }
 
 class Produto{
