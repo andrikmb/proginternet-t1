db5cc9cc66ed2f40b596238b4d248119e1d062d6
Andrik Machado Batista <andrikmb@gmail.com>
2022-04-14 20:35:06 -0300

    eliminando dados do login desnecessarios

--- cliente.php
+++ cliente.php
@@ -12,14 +12,7 @@ class Cliente {
     private int $id=0;
     //    `nome` varchar(255) COLLATE utf8_bin NOT NULL,
     private string $nome="";
-    //    `cpf_cnpj` varchar(14) COLLATE utf8_bin NOT NULL,
-    private string $cpf_cnpj="";
-    //    `email` varchar(255) COLLATE utf8_bin NOT NULL,
-    private string $email="";
-    //    `criacao` DATETIME 2022-04-27 19:35:41
-    private string $criacao="";
-    //    `modificado` DATETIME 2022-04-27 19:35:41
-    private string $modificado="";
+    private string $senha="";
     
     function create(){
         require 'functions.php';
