0008ff5d3cc0a9f0306445ea763d044bed9b81dc
Andrik Machado Batista <andrikmb@gmail.com>
2022-04-16 19:59:42 -0300

    remoção de form errado no login

--- form_login.php
+++ form_login.php
@@ -21,44 +21,6 @@ include 'header.php';
     </div>
 </form>
 
-
-<div class="container">
-    <div class="card" >
-        <div class="card-body" >
-    <form class="mb-2" >
-    <!-- Email input -->
-    <div class="form-outline mb-2 text-dark">
-        <input type="email" id="form1Example1" class="form-control" />
-        <label class="form-label" for="form1Example1">Email address</label>
-    </div>
-
-    <!-- Password input -->
-    <div class="form-outline mb-2 text-dark">
-        <input type="password" id="form1Example2" class="form-control" />
-        <label class="form-label" for="form1Example2">Password</label>
-    </div>
-
-    <!-- 2 column grid layout for inline styling -->
-    <div class="row mb-2 text-dark">
-        <div class="col d-flex justify-content-center">
-        <!-- Checkbox -->
-        <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
-            <label class="form-check-label" for="form1Example3"> Remember me </label>
-        </div>
-        </div>
-
-        <div class="col">
-        <!-- Simple link -->
-        <a href="#!">Forgot password?</a>
-        </div>
-    </div>
-
-    <!-- Submit button -->
-    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-    </form>
-    </div>  </div>
-</div>
 <?php
 include 'footer.php';
 ?>
